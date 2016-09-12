477cc21a144212abb9739bd6a699ca504bff6590
Saem <ottosigma@gmail.com>
2016-09-12 23:23:07 +0300

    Styles file

--- src/main/java/com/sameperson/controller/MainController.java
+++ src/main/java/com/sameperson/controller/MainController.java
@@ -6,10 +6,13 @@ import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 public class MainController {
-    @FXML private TextField firstName;
-    @FXML private Text heyText;
+    @FXML
+    private TextField firstName;
+    @FXML
+    private Text heyText;
 
     public void handleHey(ActionEvent actionEvent) {
         heyText.setText(String.format("Hey, %s!", firstName.getText()));
     }
+
 }
